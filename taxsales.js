0c73c033b1200c7d29864ab6a4a27e52139b70d0
Amy <amy@lighthouselabs.ca>
2017-07-26 18:42:03 +0000

    Adding working version of TaxSales assignment

--- taxsales.js
+++ taxsales.js
@@ -10,21 +10,21 @@ var salesTaxRates = {
 };
 
 var companySalesData = [
-  {
-    name: "Telus",
-    province: "BC",
-    sales: [ 100, 200, 400 ]
-  },
-  {
-    name: "Bombardier",
-    province: "AB",
-    sales: [ 80, 20, 10, 100, 90, 500 ]
-  },
-  {
-    name: "Telus",
-    province: "SK",
-    sales: [ 500, 100 ]
-  }
+{
+  name: "Telus",
+  province: "BC",
+  sales: [ 100, 200, 400 ]
+},
+{
+  name: "Bombardier",
+  province: "AB",
+  sales: [ 80, 20, 10, 100, 90, 500 ]
+},
+{
+  name: "Telus",
+  province: "SK",
+  sales: [ 500, 100 ]
+}
 ];
 
 
@@ -32,27 +32,36 @@ var companySalesData = [
 function calculateSalesTax(salesData, taxRates) {
  finalResult = {};
 
-  for (var i = 0; i < salesData.length; i++) {
+ for (var i = 0; i < salesData.length; i++) {
     var currentSalesData = salesData[i];
     var name = salesData[i]['name'];
     var salesSum = salesTotal(currentSalesData.sales);
     var province = currentSalesData.province;
     var provinceTax = salesTaxRates[currentSalesData.province];
     var taxTotal = salesSum * provinceTax;
-    finalResult[name] = {totalSales: salesSum, totalTaxes: taxTotal}
-   }
 
-    console.log(finalResult);
+
+    if (finalResult[name]) {
+      finalResult[name].totalSales = finalResult[name].totalSales + salesSum;
+      finalResult[name].totalTaxes = finalResult[name].totalTaxes + taxTotal;
+    } else {
+      finalResult[name] = {totalSales: salesSum, totalTaxes: taxTotal}
+  }
+
 }
- calculateSalesTax(companySalesData);
+
+console.log(finalResult);
+
+}
+calculateSalesTax(companySalesData);
 
 
 function salesTotal (salesData){
 //take in one object, loop through sales data and reduce to single number, sum
- var sum = 0;
+  var sum = 0;
 
-    sum = salesData.reduce(function(a, b){return a+b;});
-    return sum;
+  sum = salesData.reduce(function(a, b){return a+b;});
+  return sum;
 
-  }
+}
 
